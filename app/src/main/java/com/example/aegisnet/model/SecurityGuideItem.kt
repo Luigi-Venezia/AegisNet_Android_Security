209570d3c2b40587b0f994275724a903f7ddbec6
Luigi Venezia <luigi.venezia001@studenti.uniparthenope.it>
2026-09-14 00:24:41 +0200

    Delete app directory

--- app/src/main/java/com/example/aegisnet/model/SecurityGuideItem.kt
+++
@@ -1,7 +0,0 @@
-package com.example.aegisnet.model
-
-//Singolo elemento statico della Security Guide
-data class SecurityGuideItem(
-    val title: String,
-    val explanation: String,
-    val tips: List<String>)
--- app/src/release/java/com/example/aegisnet/security/AegisNetLogging.kt
+++
@@ -1,5 +0,0 @@
-package com.example.aegisnet.security
-import okhttp3.OkHttpClient
-/* Usato nella Release:
-   logging HTTP completamente disabilitato */
-internal fun OkHttpClient.Builder.configureAegisNetLogging(): OkHttpClient.Builder= this
